cdd321f48d981777131aa0949bf66b5aaeab78c8
Anna2985 <hshiting58@gamil.com>
2024-05-23 17:35:33 +0800

    加入專案檔案。

---
+++ LINQ_SELECT/select.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_SELECT
+{
+    class select
+    {
+        static void Main()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student{Name = "Alice",Age = 28},
+                new Student{Name = "Bob", Age = 20}
+            };
+
+            var result = from student in students
+                         where student.Age == 20
+                         select student.Name;
+
+            Console.WriteLine("20歲的學生");
+
+            foreach (var name in result)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ LINQ學習/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace LINQ學習
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = new List<int> { 1, 2, 2, 3, 3, 4, 5, 5, 5, 5 };
+
+            var distinctnumber = numbers.Distinct();
+
+            Console.WriteLine("列出非重複數字");
+
+            foreach (var number in distinctnumber)
+            {
+                Console.WriteLine(number);
+            }
+        }
+           
+    }
+}
